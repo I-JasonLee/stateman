78de2bf1b78beafb5a4a4dfbc92b637e90ca4ace
Jason <1123150116@global.ac.id>
2026-02-27 02:16:27 +0700

    flutter clean setelah

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -29,7 +29,7 @@ class MyHomePage extends StatelessWidget {
               value.increment();
             },
             tooltip: 'Increment',
-            child: const Icon(Icons.add)
+            child: const Icon(Icons.add),
           ),
         );
       },
